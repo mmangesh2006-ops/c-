f546f0893f6f333bea7e75206b7901656a05f792
mmangesh2006-ops <mmangesh2006@gmail.com>
2026-05-19 21:15:14 +0530

    Correct stack comparison logic in areEqual function
    Fix comparison loop in areEqual function to use size() method.

--- 2stacksareequalornot.cpp
+++ 2stacksareequalornot.cpp
@@ -22,7 +22,7 @@ bool areEqual(Stack s1, Stack s2) {
     if (s1.top != s2.top)
         return false;
 
-    for (int i = 0; i <= s1.top; i++) {
+    for (int i = 0; i <= s1.size(); i++) {
         if (s1.arr[i] != s2.arr[i])
             return false;
     }
